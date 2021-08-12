0429e9123179c4b001fe267bfaf712afa15b5e25
Olekoster <olegk1576@yandex.ru>
2021-08-13 00:31:59 +0300

    added scrolling between divs with js

---
+++ index.js
@@ -0,0 +1,30 @@
+function scrollBetweenInfo(event){
+    event.preventDefault;
+    switch(event.target.className){
+        case('introduction'):
+            if(event.deltaY == 102){
+                location.href = '#';//required to work around a bug in WebKit (Chrome / Safari)
+                location.href = '#works';
+                console.log("scrolled");
+            }
+            break;
+        case('myWorks'):
+            if(event.deltaY == 102){
+                location.href = '#';//required to work around a bug in WebKit (Chrome / Safari)
+                location.href = '#feedback';
+            }else{
+                location.href = '#';//required to work around a bug in WebKit (Chrome / Safari)
+                location.href = '#intro';
+            }
+            break;
+        case('feedbackForm'):
+            if(event.deltaY == -102){
+                location.href = '#';//required to work around a bug in WebKit (Chrome / Safari)
+                location.href = '#works';
+            }
+            break;
+    }
+}
+
+const element = document.querySelector('main');
+element.onwheel = scrollBetweenInfo;
