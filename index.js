e5442acf5fea16a22761fbf3e8434ce98921a336
Olekoster <olegk1576@yandex.ru>
2021-08-22 02:18:21 +0300

    deleted unnecesarry lines of code

--- index.js
+++ index.js
@@ -50,14 +50,11 @@ const photos = document.getElementById('photos');
 photos.addEventListener('mouseover', function(event){
     if(photos.style.background != ''){
         document.getElementById('showButton').hidden = false;
-        /* event.target.style.background = 'rgba(0,0,0,.5)'; */
         photos.style.webkitFilter = "brightness(60%)";
-        document.getElementById('showButton').style.webkitFilter = "";
     }
 });
 
 photos.addEventListener('mouseout', function(event){
     document.getElementById('showButton').hidden = true;
-    /* event.target.style.background = ''; */
     photos.style.webkitFilter = "";
 });
